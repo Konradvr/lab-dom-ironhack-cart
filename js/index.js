3fbed82192417d415ace01340ba569b2da026251
Konrad Ribbentrop <konradvr.business@gmail.com>
2022-08-16 13:27:15 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,23 +1,33 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  // console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector(".price span").innerHTML 
+  // console.log(price)
+  const quantity = product.querySelector(".quantity input").value
+  // console.log(quantity)
+  let subtotal = product.querySelector(".subtotal span")
+  subtotal.innerHTML = price * quantity //  the .innerHTML makes not only chaning the value of the subtotal 
+  // in the console it makes changing it in the <span> to ! so uu need to use .innerHTML there 
+  // console.log(subtotal)
+  // console.log(subtotal)
+  let totalPrice = document.querySelector("#total-value span") //use innerText to acces in the span
+  let allSubtotal = document.querySelectorAll(".subtotal span")
+  let sum = 0; 
+  for(let i = 0; i < allSubtotal.length; i++){
+    sum += parseFloat(allSubtotal[i].innerHTML) 
+  }
+  totalPrice.innerText = sum
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+  // console.log()
+  const allProductClasses = document.getElementsByClassName("product")
+  for(let i = 0; i < allProductClasses.length; i++){
+   updateSubtotal(allProductClasses[i])
+  // console.log(allProductClasses)
+  }
 }
 
 // ITERATION 4
